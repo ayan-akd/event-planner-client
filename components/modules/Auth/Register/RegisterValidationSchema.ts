b27d70ded67b9021ebba83ca145091979f69b22b
hammadsadi <hammad.sadi@yahoo.com>
2025-05-02 15:14:26 +0600

    Update Login and Register Form Validation Schema

--- components/modules/Auth/Register/RegisterValidationSchema.ts
+++ components/modules/Auth/Register/RegisterValidationSchema.ts
@@ -6,7 +6,7 @@ export const RegisterValidationSchema = z.object({
     .email("Invalid email format"),
   password: z
     .string({ required_error: "User Password is Required!" })
-    .min(8, "Password Must be 8 Characters"),
+    .min(6, "Password Must be 8 Characters"),
   name: z.string({ required_error: "Name is Required!" }),
   username: z.string({ required_error: "Username is Required!" }),
 });
