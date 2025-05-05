5875c31d967498ebe6458099c9384ea70356d743
hammadsadi <hammad.sadi@yahoo.com>
2025-05-06 01:37:24 +0600

    fetch upcoming events from database and update card UI

--- app/(commonLayout)/(home)/page.tsx
+++ app/(commonLayout)/(home)/page.tsx
@@ -3,15 +3,22 @@ import HowItsWork from "@/components/modules/Home/HowItsWork/HowItsWork";
 import UpcomingEvents from "@/components/modules/Home/UpcomingEvents/UpcomingEvents";
 import FaqSection from "@/components/modules/Home/Faq/FaqSection";
 import NewsLetter from "@/components/modules/Home/Newsletter/NewsLetter";
-import { getAdminSelectedEvents } from "@/services/Event";
+import { getAdminSelectedEvents, getAllEvents } from "@/services/Event";
 import React from "react";
+import { TEvent } from "@/types/event.type";
 
 const HomePage = async () => {
   const { data } = await getAdminSelectedEvents();
+  const { data: events } = await getAllEvents(null);
+  const upcomingEvents = events?.result?.filter((item: TEvent) => {
+    const currentDate = new Date();
+    const startDate = new Date(item?.startDate);
+    return startDate > currentDate;
+  });
   return (
     <div>
       <Hero data={data} />
-      <UpcomingEvents />
+      <UpcomingEvents events={upcomingEvents} />
       <HowItsWork />
       <FaqSection />
       <NewsLetter />
