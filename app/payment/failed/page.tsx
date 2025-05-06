883f7cffecde955c863921fe84f9e9162b10a5bb
ayan-akd <ayankumar.akd@gmail.com>
2025-05-07 00:37:00 +0600

    feat: Implement payment failed page

--- app/payment/failed/page.tsx
+++ app/payment/failed/page.tsx
@@ -1,43 +1,31 @@
-// import React from 'react';
+"use client";
 
-// const page = () => {
-//   return (
-//     <div>
-//       <h1>fail</h1>
-//     </div>
-//   );
-// };
+import { useParams, useRouter } from "next/navigation";
+import { AlertCircle } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-// export default page;
+const PaymentFailPage = () => {
+  const router = useRouter();
+  const {tran_id} = useParams();
 
-// "use client";
+  console.log("Transaction ID:", tran_id);
 
-// import { useParams, useRouter } from "next/navigation";
-// import { AlertCircle } from "lucide-react";
-// import { Button } from "@/components/ui/button";
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+      <AlertCircle className="text-red-600 w-16 h-16 mb-4" />
+      <h1 className="text-2xl font-bold text-red-600">Payment Failed</h1>
+      <p className="mt-2 text-gray-600">
+        Unfortunately, your transaction could not be completed.
+      </p>
+      <p className="text-sm text-gray-400 mt-1">
+        Transaction ID: {tran_id || "Unknown"}
+      </p>
 
-// const PaymentFailPage = () => {
-//   const router = useRouter();
-//   const {tran_id} = useParams();
+      <Button className="mt-6" onClick={() => router.push("/")}>
+        Go to Home
+      </Button>
+    </div>
+  );
+};
 
-//   console.log("Transaction ID:", tran_id);
-
-//   return (
-//     <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
-//       <AlertCircle className="text-red-600 w-16 h-16 mb-4" />
-//       <h1 className="text-2xl font-bold text-red-600">Payment Failed</h1>
-//       <p className="mt-2 text-gray-600">
-//         Unfortunately, your transaction could not be completed.
-//       </p>
-//       <p className="text-sm text-gray-400 mt-1">
-//         Transaction ID: {tran_id || "Unknown"}
-//       </p>
-
-//       <Button className="mt-6" onClick={() => router.push("/")}>
-//         Go to Home
-//       </Button>
-//     </div>
-//   );
-// };
-
-// export default PaymentFailPage;
+export default PaymentFailPage;
