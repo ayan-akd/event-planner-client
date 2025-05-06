d7c0b050a9bc0031c7550bee1a003cc6cfbc5923
hammadsadi <hammad.sadi@yahoo.com>
2025-05-06 14:39:51 +0600

    add review creation functionality and display all reviews for specific event

--- components/modules/EventDetails/Reviews/Reviews.tsx
+++ components/modules/EventDetails/Reviews/Reviews.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import CreateReview from "./CreateReview";
-import { reviewsData, TFakeReviewType } from "@/faker/reviews";
 import ReviewItem from "./ReviewItem";
+import { getSpecificReviewsForSpecificEvent } from "@/services/Review";
+import { TReview } from "@/types/review.type";
 
-const Reviews = async () => {
+const Reviews = async ({ eventId }: { eventId: string }) => {
+  const { data } = await getSpecificReviewsForSpecificEvent(eventId);
   return (
     <div className="max-w-lg mt-6">
       <div className="flex justify-around mb-4">
-        <h4 className="font-bold">Reviews ( {0} )</h4>
+        <h4 className="font-bold">Reviews ( {data?.length || 0} )</h4>
         <CreateReview />
       </div>
 
-      {reviewsData?.length > 0 ? (
+      {data?.length > 0 ? (
         <>
-          {reviewsData?.map((review: TFakeReviewType) => (
+          {data?.map((review: TReview) => (
             <ReviewItem key={review.id} review={review} />
           ))}
         </>
